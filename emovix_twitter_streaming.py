bce676ea7a0199730f891b16be368af94e5b519f
Jordi Vilaplana <gatasuna@gmail.com>
2015-09-25 14:17:28 +0200

    Update emovix_twitter_streaming.py

--- emovix_twitter_streaming.py
+++ emovix_twitter_streaming.py
@@ -9,14 +9,15 @@ import logging
 
 logging.basicConfig(filename='emovix_twitter_streaming.log',level=logging.WARNING)
 
-#Variables that contains the user credentials to access Twitter API
+# Configuration parameters
 access_token = ""
 access_token_secret = ""
 consumer_key = ""
 consumer_secret = ""
+database_name = ""
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['emovix']
+client = None
+db = None
 
 class CustomStreamListener(tweepy.StreamListener):
     def __init__(self, api):
@@ -26,10 +27,13 @@ class CustomStreamListener(tweepy.StreamListener):
 
     def on_data(self, data):
         tweet = json.loads(data)
-        if tweet.limit:
-            print 'Limit notice received'
-            logging.debug('Limit notice received')
+
+        # This code ignores limit notices
+        # https://dev.twitter.com/streaming/overview/messages-types#limit_notices
+        if tweet.get('limit'):
+            logging.debug('Limit notice received: ' + str(tweet['limit']['track']))
             return True
+
         twitterStatus = db.twitterStatus
         self.db.twitterStatus.update(tweet, tweet, upsert=True)
         return True
@@ -45,12 +49,26 @@ class CustomStreamListener(tweepy.StreamListener):
 
 if __name__ == '__main__':
     logging.debug('emovix_twitter_streaming.py starting ...')
+
+    # Load configuration
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+        access_token = config['access_token']
+        access_token_secret = config['access_token_secret']
+        consumer_key = config['consumer_key']
+        consumer_secret = config['consumer_secret']
+        database_name = config['database_name']
+
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client[database_name]
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
     while True:
         try:
+            logging.warning('Connectiong ...')
             stream = tweepy.streaming.Stream(auth, CustomStreamListener(api))
             stream.filter( locations = [-180, -90, 180, 90] )
         except Exception as e:
